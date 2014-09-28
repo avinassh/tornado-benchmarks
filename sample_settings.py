d61966920053e41c0aabe9a3c6dd74579aaa34bf
avinassh <avinashsajjan@gmail.com>
2014-09-28 22:48:55 +0530

    added sample settings

---
+++ sample_settings.py
@@ -0,0 +1,15 @@
+# all the settings, auth etc.
+
+import os
+
+# put your mongodb username and password 
+# "mongodb://username:password@staff.mongohq.com:someport/mongodb_name"
+# following is obtained from https://app.mongohq.com/username/mongo/mongodbname/admin
+
+MONGO_URL = "mongodb://username:password@kahana.mongohq.com:10086/mongodb_name"
+
+application_handler_setttings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            debug=True,
+            )
